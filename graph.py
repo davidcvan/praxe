eb83d0b80df9a6207ad537f1d1bed1ce98eb2d01
David Cvan <david.cvan64@gmail.com>
2018-05-23 20:34:22 +0200

    Uz to funguje. Jeeeej

--- graph.py
+++ graph.py
@@ -1,19 +1,32 @@
-import numpy as np
 import matplotlib.pyplot as plt
 lines = []
-with open("./data/fabric8-analytics-worker.coverage.0.txt") as f:
-    for line in f:
-        words = line.split()
-        lines.append(words)
-del lines[0]
-del lines[0]
-del lines[-1]
-del lines[-1]
-for i in range(len(lines)):
-    loc = int(lines[i][1])
-    tested_loc = int(lines[i][2])
-    plt.plot([1,2,3],[loc,loc,loc])
-    plt.plot([1,2,3],[tested_loc,tested_loc,tested_loc])
-    plt.show()
-print(lines)
+dict = {}
 
+def opening(name):
+    with open(name,"r") as f:
+        lines.append([name])
+        for line in f:
+            if line[0] == "/":
+                words = line.split()
+                lines.append(words)
+
+def draw_graph(file_name):
+    x_axis = []
+    loc = []
+    tested_loc = []
+    for i in range(len(lines)):
+        if len(lines[i])==1:
+            x_axis.append(lines[i][0])
+        elif lines[i][0] == file_name:
+            loc.append(int(lines[i][1]))
+            tested_loc.append(int(lines[i][2]))
+    print(x_axis, loc, tested_loc)
+    plt.title(file_name)
+    plt.plot(x_axis, loc, label = "loc")
+    plt.plot(x_axis,tested_loc, label = "tested_loc")
+    plt.legend()
+    plt.show()
+
+opening("./data/fabric8-analytics-worker.coverage.0.txt" )
+opening("./data/fabric8-analytics-worker.coverage.1.txt" )
+draw_graph("/f8a_worker/f8a_worker/base.py")
