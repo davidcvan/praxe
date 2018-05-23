46708b7a042710132974e4d9b8219cfdaeaa3ea9
David Cvan <david.cvan64@gmail.com>
2018-05-23 11:36:17 +0200

    showing some graph

--- graph.py
+++ graph.py
@@ -1,11 +1,19 @@
-import datetime
-from matplotlib import pyplot as plt
-loc = [1200, 1245, 1357]
-dates = [1.1,3.1,5.1]
+import numpy as np
+import matplotlib.pyplot as plt
+lines = []
+with open("./data/fabric8-analytics-worker.coverage.0.txt") as f:
+    for line in f:
+        words = line.split()
+        lines.append(words)
+del lines[0]
+del lines[0]
+del lines[-1]
+del lines[-1]
+for i in range(len(lines)):
+    loc = int(lines[i][1])
+    tested_loc = int(lines[i][2])
+    plt.plot([1,2,3],[loc,loc,loc])
+    plt.plot([1,2,3],[tested_loc,tested_loc,tested_loc])
+    plt.show()
+print(lines)
 
-tested_loc = [500, 650, 670]
-
-
-plt.plot(dates,tested_loc)
-plt.plot(dates,loc)
-plt.show()
