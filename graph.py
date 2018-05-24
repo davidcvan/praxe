37129b1b266226c6657d3cdff1843004c1c45143
tkrejzlik <39521409+tkrejzlik@users.noreply.github.com>
2018-05-24 14:45:39 +0200

    Add files via upload

--- graph.py
+++ graph.py
@@ -14,20 +14,25 @@ def draw_graph(file_name):
     tested_loc = []
     for i in range(len(lines)):
         if len(lines[i])==1:
-            x_axis.append(lines[i][0])
+            name = lines[i][0]
+            x_axis.append(name[-14:])
         elif lines[i][0] == file_name:
             loc.append(int(lines[i][1]))
             tested_loc.append(int(lines[i][1]) - int(lines[i][2]))
     print(x_axis, loc, tested_loc)
     plt.title(file_name)
     plt.plot(x_axis, loc, label = "loc")
+    plt.xticks(x_axis, rotation='vertical')
+    plt.xticks(size=7)
+    plt.yticks(size=10)
     plt.plot(x_axis,tested_loc, label = "tested_loc")
+
     plt.legend()
-    plt.savefig(file_name.replace("/","_")+".png")
+    plt.savefig(file_name.replace("/", "_") + ".png")
     plt.show()
 
 lines = []
-files = ["./data/fabric8-analytics-worker.coverage.0.txt","./data/fabric8-analytics-worker.coverage.1.txt","./data/fabric8-analytics-worker.coverage.2.txt","./data/fabric8-analytics-worker.coverage.3.txt"]
+files = ["/home/tkrejzlik/Desktop/data/praxe-master/data/praxe-master/data/praxe-master/data/fabric8-analytics-worker.coverage.0.txt","/home/tkrejzlik/Desktop/data/praxe-master/data/praxe-master/data/praxe-master/data/fabric8-analytics-worker.coverage.1.txt","/home/tkrejzlik/Desktop/data/praxe-master/data/praxe-master/data/praxe-master/data/fabric8-analytics-worker.coverage.2.txt","/home/tkrejzlik/Desktop/data/praxe-master/data/praxe-master/data/praxe-master/data/fabric8-analytics-worker.coverage.3.txt"]
 
 for file in files:
     opening(file)
