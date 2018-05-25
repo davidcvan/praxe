c6393edafe25117068950cc2687326f00349fe73
David Cvan <david.cvan64@gmail.com>
2018-05-25 10:47:57 +0200

    Zapisuje hashe do seznamu

--- git_script.py
+++ git_script.py
@@ -1,6 +1,11 @@
 import os
 
+lines = []
 os.system("cd ./fabric8-analytics.github.io; git log --pretty=oneline > logs.txt")
 with open("./fabric8-analytics.github.io/logs.txt","r") as f:
     for line in f:
-        print(f.readline())
+        splitted_line = line.split()
+        if splitted_line[1] == "Dashboard" and len(splitted_line) <= 3:
+            lines.append(splitted_line)
+    f.close()
+print(lines, len(lines))
