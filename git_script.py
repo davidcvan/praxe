e06a75867fc548ef57f2b0482490121e714f744e
David Cvan <david.cvan64@gmail.com>
2018-05-29 09:38:16 +0200

    hotovo

--- git_script.py
+++ git_script.py
@@ -1,13 +1,38 @@
 import os
+import graph
+commits = []
+file = "fabric8-analytics.github.io/dashboard/f8a-server-backbone.coverage.txt"
 
-lines = []
-os.system("cd ./fabric8-analytics.github.io; git log --pretty=oneline > logs.txt")
-with open("./fabric8-analytics.github.io/logs.txt","r") as f:
-    for line in f:
-        splitted_line = line.split()
-        if splitted_line[1] == "Dashboard" and len(splitted_line) <= 3:
-            lines.append(splitted_line)
-    f.close()
-print(lines, len(lines))
-hash = lines[0][0]
-os.system("cd ./fabric8-analytics.github.io; git checkout %s"% (hash))
+def opening():
+    os.system("cd fabric8-analytics.github.io; git log --pretty=oneline > logs.txt")
+
+    with open("./fabric8-analytics.github.io/logs.txt","r") as f:
+        for line in f:
+            splitted_line = line.split()
+            if splitted_line[1] == "Dashboard" and len(splitted_line) == 3:
+                commits.append(splitted_line)
+        f.close()
+    print(commits, len(commits))
+
+def checkout():
+    for commit in commits:
+        os.system("cd ./fabric8-analytics.github.io; git checkout %s" % (commit[0]))
+        print(os.path.exists(file))
+        if os.path.exists(file):
+            graph.opening(file, commit[2])
+
+
+opening()
+checkout()
+print(len(graph.lines))
+for file in graph.lines:
+    if len(file) > 1:
+        graph.draw_graph(file[0])
+
+
+'''
+        graph.opening(file)
+    else:
+        print(os.listdir("fabric8-analytics.github.io/dashboard/"))
+print(graph.lines)
+'''
