539e3043c0af915b599684712d455e2d2fb2a735
David Cvan <david.cvan64@gmail.com>
2018-05-30 10:08:06 +0200

    Uprava formatovani

--- git_script.py
+++ git_script.py
@@ -1,17 +1,15 @@
 import os
 import graph
-import sys
 
-arguments = sys.argv
 commits = []
 file = "fabric8-analytics.github.io/dashboard/f8a-server-backbone.coverage.txt"
 
 
 def opening():
-    os.system("cd fabric8-analytics.github.io; git log --pretty=oneline > "
-              "logs.txt")
+    os.system("cd fabric8-analytics.github.io; git log --pretty=oneline > \
+              logs.txt")
 
-    with open("./fabric8-analytics.github.io/logs.txt", "r") as f:
+    with open("fabric8-analytics.github.io/logs.txt", "r") as f:
         for line in f:
             splitted_line = line.split()
             if splitted_line[1] == "Dashboard" and len(splitted_line) == 3:
@@ -32,7 +30,6 @@ def checkout():
 
 opening()
 checkout()
-print(arguments)
 for file in graph.lines:
     if len(file) > 1:
         graph.draw_graph(file[0])
